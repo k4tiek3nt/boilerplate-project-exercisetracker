97ccb8f0c4852fc458c011588e2c0ce26b47efd1
Katherine Kent <k4tiek3nt1981@gmail.com>
2023-02-08 23:38:04 +0000

    Index.js - added error handling for router accessing file system

--- index.js
+++ index.js
@@ -29,6 +29,19 @@ const fs = require("fs");
 const path = require("path");
 const router = express.Router();
 
+// Error handling for router file access 
+router.get("/file/*?", function (req, res, next) {
+  if (req.params[0] === ".env") {
+    return next({ status: 401, message: "ACCESS DENIED" });
+  }
+  fs.readFile(path.join(__dirname, req.params[0]), function (err, data) {
+    if (err) {
+      return next(err);
+    }
+    res.type("txt").send(data.toString());
+  });
+});
+
 // listener that alerts when app is connected
 const listener = app.listen(port, () => {
   console.log('Your app is listening on port ' + listener.address().port)
