90a9dc3fd8a2101bb2d0dcf5cbbcb7a23a52fcdd
Katherine Kent <k4tiek3nt1981@gmail.com>
2023-02-27 19:37:42 +0000

    myApp - removed require('dotenv').config(); because was not being used. Added default value to date in model.

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 'use strict'; // Avoids undefined variables
 
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
 
 // avoids choke on 204 (some legacy browsers)
 app.use(cors({ optionsSuccessStatus: 200 })); 
 
 // Info for static files, allows CSS to render
-app.use(express.static('public'))
+app.use(express.static('public'));
 
 // Renders WebPage "index.html"
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+  res.sendFile(__dirname + '/views/index.html');
 });
+
+//middleware
+app.use(express.json());
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -63,6 +66,7 @@ mongoose.connect(
   { 
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    useFindAndModify: false 
   },
   (err) => {
     if (err) {
@@ -78,7 +82,58 @@ const User = require("./myApp.js").UserModel;
 
 // new API endpoint for creating new user
 app.post('/api/users', function(req, res) {
-// Defines username to use based on user input
+
+// Defines username based on user input
+const username = req.body.username;
+
+if (username === '') {
+  console.log('No username entered.');
+  return res.json({ error: 'You clicked submit without entering a username. Please enter a username and then click submit.' });
+}
+
+ User.findOne({ username: username }, function (err, user) {
+  if (!err && user === null) {
+   console.log('Good choice of username, it is unique! You entered: ' + username);
+   let newUser = new User({
+    username: username
+   });
+
+   newUser.save(function (err, user) {
+    if (!err) {
+     console.log('User added: ' + user);
+     return res.json({
+     username: user.username,
+     _id: user._id,
+     });
+    }
+   });
+  } else {
+   console.log('Duplicate username. User not added, please try again with unique username.');
+   return res.json({ error: 'Username already exists, please try again.' });
+  }
+ });
+});
+
+// new API endpoint to return all saved users
+app.get('/api/users', function (req, res) {
+User.find({})
+  .select('_id, username')
+  .exec(function(err, users) {
+  if (!err) {
+   console.log('User List Found.');
+   res.json(users);
+  } else {
+  console.log(err);
+   return res.json(err);
+  } 
+});
+});
+  
+console.debugger;
+  
+  
+ /* 
+  // Defines username based on user input
 const username = req.body.username;
 let newUser = new User({username: username});
 newUser.save(function(err, user) {
@@ -87,56 +142,200 @@ newUser.save(function(err, user) {
     res.send("New user not added, please try again. Be sure to include a username.");
   } 
   else{
+    //Print saved user to console
     console.log("User added: " + user);
-    res.send({ _id: user._id, username: user.username });
-  }  
-  });
+    //Return json info
+    res.json({ _id: user._id, username: user.username });
+  }
+},
+)
 });
 
+
+
+ // new API endpoint to return added user
+app.get('/api/users?', function(req, res) {
+  const id = req.params._id;
+  User.findById(id, function(err, user) {
+    if (err) res.send("Invalid ID");
+    res.send(user);
+    res.json(user);
+  }); 
+});
+*/
+/* User.findOne({}, function(err, result) {
+    if (err) throw err;
+    console.log(result.name); */
+/*
 // new API endpoint to return all saved users
 app.get('/api/users', function(req, res) {
 User.find({})
-  .select('username _id')
+  .select('_id, username')
   .exec(function(err, users) {
     if (err) res.send("No users found");
     res.send(users);
   }); 
-});    
+});    */
 
 // new API endpoint to Add Exercises to Log
-app.post('/api/users/:_id/exercises', function(req, res) {
+
+app.post('/api/users/:_id/exercises', function(req, res){
   const id = req.params._id;
-  User.findById(id, function(err, user) {
-    if (err) res.send("Invalid ID");
-    const description = req.body.description;
-    const duration = req.body.duration && 
-      !Number.isNaN(Number(req.body.duration)) ? 
-      Number(req.body.duration) : 
-      res.send(`Enter valid input for 'duration'. 
-      This should be a number. For example for a 30 minute 
-      exercise, please input 30.`
-    );
+  const request = req.body;
+  const description = request.description;
+    const duration = request.duration && !Number.isNaN(Number(request.duration)) ? Number(request.duration) : res.send(`Enter valid input for 'duration'. This should be a number. For example for a 30 minute exercise, please input 30.`);
     
     // if no input for date, use today
-    const date = !req.body.date ? new Date().toDateString() : req.body.date ? new Date(req.body.date).toDateString() : 
-      res.send(`Enter a valid input for 'date'`);
+    const date = !request.date ? new Date() : request.date ? new Date(request.date) : 
+      res.send(`Enter a valid input for 'date'.`);
+
+    let dateString = date.toString();
       
     let newExercise = {
       description: description,
       duration: duration,
-      date: date
+      date: dateString
     };
     
-    user.log.push(newExercise);
-    user.save((err, user) => {
+
+User.findByIdAndUpdate(id, {log:[ newExercise]}, {new: true}, (err, user) => {
     if (err) res.send("New exercise not added, please try again.");
-    res.send({username: user.username, description: description, duration: duration, date: date, _id: id});
+    console.log(user) 
+    res.json({username: user.username, description: user.description, duration: user.duration, date: user.date, _id: id});
     });
-  });
+   
 });
+
+app.get('/api/users/:_id/exercises', function (req, res) {
+const id = req.params._id;
+ res.redirect('/api/users/' + id + '/logs');
+});
+
+app.get('/api/users/:_id/logs/:from?/:to?/:limit?', function (req, res) {
+ let id = req.params._id;
+  console.log(id);
+ let dateFrom = req.query.from;
+ let dateTo = req.query.to;
+ let limit = (req.query.limit !== undefined ? parseInt(req.query.limit) : 0);
+ let query;
+
+ if (typeof dateFrom != 'undefined' && typeof dateTo != 'undefined' && typeof id != 'undefined' && typeof limit != 'undefined') query = { date: { $gte: dateFrom, $lte: dateTo }, _id: id, limit: limit };
+ if (typeof dateFrom != 'undefined' && typeof dateTo == 'undefined' && typeof id != 'undefined' && limit != 'undefined') query = { date: { $gte: dateFrom }, _id: id, limit: limit };
+ if (typeof dateFrom == 'undefined' && typeof dateTo != 'undefined' && typeof id != 'undefined' && limit != 'undefined') query = { date: { $lte: dateTo },  _id: id, limit: limit };
+ if (typeof dateFrom != 'undefined' && typeof dateTo == 'undefined' && typeof id != 'undefined' && limit == 'undefined') query = { date: { $gte: dateFrom }, _id: id };
+ if (typeof dateFrom == 'undefined' && typeof dateTo != 'undefined' && typeof id != 'undefined' && limit == 'undefined') query = { date: { $lte: dateTo }, _id: id };
+ if (typeof dateFrom == 'undefined' && typeof dateTo == 'undefined' && typeof id != 'undefined' && limit != 'undefined') query = { _id: id, limit: limit };
+ if (typeof dateFrom == 'undefined' && typeof dateTo == 'undefined' && typeof id != 'undefined' && limit == 'undefined') query = { _id: id };
+
+ if ((dateFrom !== undefined && dateFrom !== '') &&
+  (dateTo !== undefined && dateTo !== '')
+ ) {
+ let fromDateString = query.date.$gte = new Date(dateFrom).toDateString();
+ /* checkDate with moment */
+  if (fromDateString == 'Invalid Date') {
+   return res.json({ error: 'From date was not entered in YYYY-MM-DD format, please try again.' });
+  }
+
+ let toDateString = query.date.$lte = new Date(dateTo).toDateString();
+
+/* checkDate with moment */
+  if (toDateString == 'Invalid Date') {
+   return res.json({ error: 'To date was not entered in YYYY-MM-DD format, please try again.' });
+  }
+ }
+
+ /*if (isNaN(limit)) {
+  return res.json({ error: 'limit is not a number' });
+ }*/
+
+const exerciseLog = 
+   User.find(query).exec(function (err, user) {
+    if (!err && user !== null) {
+     return res.json({
+      _id: user._id,
+      username: user.username,
+      log: {
+    description: user.description,
+        duration: user.duration,
+        date: user.date
+       },
+      count: user.length
+     });
+    }
+
+ 
+ if (!exerciseLog) return res.status(404).send('The exercise log with the given ID was not found.');
+
+ res.send(exerciseLog);
+});
+});
+      
+
+/* app.get('/api/users/:_id/logs', function (req, res) {
+ let id = req.params._id;
+ console.log(id);
+ let queryParameters = { _id: id };
+ let fromDate = req.query.from;
+ let toDate = req.query.to;
+
+ if ((fromDate !== undefined && fromDate !== '') &&
+  (toDate !== undefined && toDate !== '')
+ ) {
+  queryParameters.date = {};
+
+ let fromDateString = queryParameters.date.$gte;
+   fromDateString = new Date(fromDate).toDateString();
+  
+ /* checkDate with moment */
+
+/*  if (fromDateString == 'Invalid Date') {
+   return res.json({ error: 'From date was not entered in  YYYY-MM-DD format, please try again.' });
+  }
+
+ let toDateString = queryParameters.date.$lte;
+   toDateString = new Date(toDate).toDateString();
+
+/* checkDate with moment */
+ 
+
+/*  if (toDateString == 'Invalid Date') {
+   return res.json({ error: 'To date was not entered in YYYY-MM-DD format, please try again.' });
+  }
+ }
+
+ let limit = (req.query.limit !== undefined ? parseInt(req.query.limit) : 0);
+
+ if (isNaN(limit)) {
+  return res.json({ error: 'limit is not a number' });
+ }
+
+ User.findById(id, function (err, user) {
+  if (!err && user !== null) {
+   User.find(queryParameters).sort({ date: 'asc' }).limit(limit).exec(function (newErr, foundUser) {
+    if (!newErr && foundUser !== null) {
+     return res.json({
+      _id: user['_id'],
+      username: user['username'],
+      log: foundUser.map(function (e) {
+       return {
+        description: e.description,
+        duration: e.duration,
+        date: e.date
+       };
+      }),
+      count: foundUser.length
+     });
+    }
+   });
+  } else {
+   return res.json({ error: err });
+  }
+ });
+}); */
+
 
 // listener that alerts when app is connected
 // updated from app.listen to server.listen
 const listener = server.listen(port, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+  console.log('Your app is listening on port ' + listener.address().port);
+});
--- myApp.js
+++ myApp.js
@@ -1,5 +1,3 @@
-require('dotenv').config();
-
 // redefining mongoose for use in myApp
 const mongoose = require('mongoose');
 
@@ -9,10 +7,11 @@ const Schema = mongoose.Schema;
 // Create User Schema 
 const userSchema = new Schema({
   username: {type: String, required: true},
-  log: [{    
+  log: [{  
+    _id: false,
     description: {type: String, required: true},
     duration: {type: Number, required: true},
-    date: {type: String, required: false}
+    date: {type: String, default: Date.now, required: false}
   }]
 });
 
